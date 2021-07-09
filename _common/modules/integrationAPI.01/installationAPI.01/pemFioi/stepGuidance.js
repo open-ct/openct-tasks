491a72ec1be3b1f29651478bd424bfd1c56ae529
Rollins <996745013@qq.com>
2021-07-09 11:51:53 +0800

    '修复引导BUG'

--- _common/modules/integrationAPI.01/installationAPI.01/pemFioi/stepGuidance.js
+++ _common/modules/integrationAPI.01/installationAPI.01/pemFioi/stepGuidance.js
@@ -8,7 +8,7 @@
  * @FilePath: \bebras-tasks\_common\modules\integrationAPI.01\installationAPI.01\pemFioi\stepGuidance.js
  */
 
-class StepGuidance{
+class StepGuidance {
     /**
      * elementStepList 步骤元素列表
      * 元素参数：
@@ -35,43 +35,43 @@ class StepGuidance{
     level = "easy"
     guidanceName = ""
     stepIndex = 0
-    constructor(stepList,guidanceName){
-        let levelHash = window.location.hash?window.location.hash.replace('#',''):'easy'
-        this.level = ['easy','medium','hard'].includes(levelHash)?levelHash:'easy'
-        this.elementStepList = stepList.filter(item=>item.level.includes(this.level))
+    constructor(stepList, guidanceName) {
+        let levelHash = window.location.hash ? window.location.hash.replace('#', '') : 'easy'
+        this.level = ['easy', 'medium', 'hard'].includes(levelHash) ? levelHash : 'easy'
+        this.elementStepList = stepList.filter(item => item.level.includes(this.level))
         this.guidanceName = guidanceName
     }
-    init(){
-        if(this.getCookie()){
-            console.log('页面已经加载过指导，不再加载')
-        }else{
-            window.onload = ()=>{  
+    init() {
+        if (this.getReloadState()) {
+            console.log('页面为点击重载，不再加载步骤引导')
+        } else {
+            window.onload = () => {
                 this.guidanceBegin()
                 this.updateGuidanceStep()
-                $('#btn-box').on('click','#next-step-btn',()=>{
+                $('#btn-box').on('click', '#next-step-btn', () => {
                     this.stepIndex += 1
                     this.updateGuidanceStep()
                 })
-                $('#btn-box').on('click','#last-step-btn',()=>{
+                $('#btn-box').on('click', '#last-step-btn', () => {
                     this.stepIndex -= 1
                     this.updateGuidanceStep()
                 })
-                $('#btn-box').on('click','#complete-btn',()=>{
+                $('#btn-box').on('click', '#complete-btn', () => {
                     $('#guide-box').remove()
-                    this.setCookie()
                 })
-                $(window).resize(()=>{
+                $(window).resize(() => {
                     this.updateGuidanceStep()
                 })
-                $(document).scroll(()=>{
+                $(document).scroll(() => {
                     this.updateGuidanceStep()
                 })
             }
         }
+        this.delReloadState()
     }
-    guidanceBegin(){
+    guidanceBegin() {
         //载入引导主体
-        if($("#guide-box").length==0){
+        if ($("#guide-box").length == 0) {
             $('body').append(`<div id="guide-box">
                               <div class="guide-area">
                                  <div class="guide-tip-box" id="guide-tip">
@@ -88,17 +88,17 @@ class StepGuidance{
                            </div>`)
         }
     }
-    updateGuidanceStep(){
-        let elementTarget = this.elementStepList[this.elementStepList.findIndex(item=>item.step[this.level]==this.stepIndex)]
-        $('#guide-box > .guide-area > .guide-tip-box > .guide-tip-content > .triangle').css('left',elementTarget.tip_position.t_left)
-        $('#guide-box > .guide-area').css('width',this.getTargetWidth(elementTarget)+'px')
-                                     .css('height',this.getTargetHeight(elementTarget)+'px')
-                                     .css('top',this.getTargetTop(elementTarget)+'px')
-                                     .css('left',this.getTargetLeft(elementTarget)+'px')
-                                     .css('border-radius',elementTarget.borderRadius+'px')
-                                     .css('padding',elementTarget.padding)
-        for(let key in elementTarget.tip_position){
-            $('#guide-tip').css(key,elementTarget.tip_position[key])
+    updateGuidanceStep() {
+        let elementTarget = this.elementStepList[this.elementStepList.findIndex(item => item.step[this.level] == this.stepIndex)]
+        $('#guide-box > .guide-area > .guide-tip-box > .guide-tip-content > .triangle').css('left', elementTarget.tip_position.t_left)
+        $('#guide-box > .guide-area').css('width', this.getTargetWidth(elementTarget) + 'px')
+            .css('height', this.getTargetHeight(elementTarget) + 'px')
+            .css('top', this.getTargetTop(elementTarget) + 'px')
+            .css('left', this.getTargetLeft(elementTarget) + 'px')
+            .css('border-radius', elementTarget.borderRadius + 'px')
+            .css('padding', elementTarget.padding)
+        for (let key in elementTarget.tip_position) {
+            $('#guide-tip').css(key, elementTarget.tip_position[key])
         }
         $('#guide-box > .guide-area > .guide-tip-box > .guide-tip-content > .tip-info > span').text(elementTarget.tips)
         $('#guide-box > .guide-area > .guide-tip-box > .guide-tip-content > .button-box').html(`
@@ -107,83 +107,78 @@ class StepGuidance{
                                                                                                 ${this.elementStepList.length-1==this.stepIndex?'<div id="complete-btn" class="step-btn" style="background-color:#FDF151;border:none;"><span>我知道了！</span></div>':''}
                                                                                              `)
     }
-    getCookie(cookieName=this.guidanceName){
-        var name = cookieName + "=";
+    getReloadState() {
+        var name = "reload=";
         var ca = document.cookie.split(';');
-        for(var i=0; i<ca.length; i++) {
+        for (var i = 0; i < ca.length; i++) {
             var c = ca[i].trim();
-            if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+            if (c.indexOf(name) == 0) { return c.substring(name.length, c.length); }
         }
         return false;
     }
-    setCookie(cookieName=this.guidanceName,value='true'){
-        var expdate = new Date();   //初始化时间
-        expdate.setTime(expdate.getTime() + 5 * 60 * 1000);   //时间单位毫秒
-        document.cookie = cookieName+"="+value+";expires="+expdate.toGMTString()+";path=/";
+    delReloadState() {
+        var expdate = new Date(); //初始化时间
+        expdate.setTime(expdate.getTime() - 10000); //时间单位毫秒
+        document.cookie = "reload=" + true + ";expires=" + expdate.toGMTString() + ";path=/";
     }
-    getTargetWidth(elementTarget){
-        if(typeof elementTarget.target == 'object'){
-           let value = 0;
-           let leftValue = [2000,0]
-           for(let elementItem of elementTarget.target){
-              let elementLeft = $(elementItem).offset().left
-              if(elementLeft<leftValue[0]){
-                 leftValue[0] = elementLeft
-              }
+    getTargetWidth(elementTarget) {
+        if (typeof elementTarget.target == 'object') {
+            let value = 0;
+            let leftValue = [2000, 0]
+            for (let elementItem of elementTarget.target) {
+                let elementLeft = $(elementItem).offset().left
+                if (elementLeft < leftValue[0]) {
+                    leftValue[0] = elementLeft
+                }
 
-              if(elementLeft>leftValue[1]){
-                 leftValue[1] = elementLeft
-                 value = 0
-                 //value += $(elementItem).width()
-                 value += elementTarget.isSVG?$(elementItem).get(0).getBBox().width:$(elementItem).outerWidth()
-                 value += $(elementItem).width()
-              }
-           }
-           return value + leftValue[1] - leftValue[0]
+                if (elementLeft > leftValue[1]) {
+                    leftValue[1] = elementLeft
+                    value = 0
+                    value += elementTarget.isSVG ? $(elementItem).get(0).getBBox().width : $(elementItem).outerWidth()
+                }
+            }
+            return value + leftValue[1] - leftValue[0]
         }
-        return elementTarget.isSVG?$(elementTarget.target).get(0).getBBox().width:$(elementTarget.target).outerWidth()
+        return elementTarget.isSVG ? $(elementTarget.target).get(0).getBBox().width : $(elementTarget.target).outerWidth()
     }
     getTargetHeight(elementTarget) {
-        if(typeof elementTarget.target == 'object'){
-           let value = 0;
-           let topValue = [2000,0]
-           for(let elementItem of elementTarget.target){
-              let elementTop = $(elementItem).offset().top
-              if(elementTop<topValue[0]){
-                 topValue[0] = elementTop
-              }
+        if (typeof elementTarget.target == 'object') {
+            let value = 0;
+            let topValue = [2000, 0]
+            for (let elementItem of elementTarget.target) {
+                let elementTop = $(elementItem).offset().top
+                if (elementTop < topValue[0]) {
+                    topValue[0] = elementTop
+                }
 
-              if(elementTop>topValue[1]){
-                 topValue[1] = elementTop
-                 value = 0
-                 //value += $(elementItem).height()
-                 value += elementTarget.isSVG?$(elementItem).get(0).getBBox().height:$(elementItem).outerHeight()
-                 value += $(elementItem).height()
-              }
-           }
-           return value + topValue[1] - topValue[0]
+                if (elementTop > topValue[1]) {
+                    topValue[1] = elementTop
+                    value = 0
+                    value += elementTarget.isSVG ? $(elementItem).get(0).getBBox().height : $(elementItem).outerHeight()
+                }
+            }
+            return value + topValue[1] - topValue[0]
         }
-        return elementTarget.isSVG?$(elementTarget.target).get(0).getBBox().height:$(elementTarget.target).outerHeight()
+        return elementTarget.isSVG ? $(elementTarget.target).get(0).getBBox().height : $(elementTarget.target).outerHeight()
     }
     getTargetTop(elementTarget) {
-        if(typeof elementTarget.target == 'object'){
-           let value = 2000
-           for(let elementItem of elementTarget.target){
-              value = $(elementItem).offset().top<value?$(elementItem).offset().top:value
-           }
-           return value - parseFloat(elementTarget.padding.split(' ')[0].replace('px')) - $(document).scrollTop()
+        if (typeof elementTarget.target == 'object') {
+            let value = 2000
+            for (let elementItem of elementTarget.target) {
+                value = $(elementItem).offset().top < value ? $(elementItem).offset().top : value
+            }
+            return value - parseFloat(elementTarget.padding.split(' ')[0].replace('px')) - $(document).scrollTop()
         }
         return $(elementTarget.target).offset().top - parseFloat(elementTarget.padding.split(' ')[0].replace('px')) - $(document).scrollTop()
     }
     getTargetLeft(elementTarget) {
-        if(typeof elementTarget.target == 'object'){
-           let value = 2000
-           for(let elementItem of elementTarget.target){
-              value = $(elementItem).offset().left<value?$(elementItem).offset().left:value
-           }
-           return value - parseFloat(elementTarget.padding.split(' ')[1].replace('px'))
+        if (typeof elementTarget.target == 'object') {
+            let value = 2000
+            for (let elementItem of elementTarget.target) {
+                value = $(elementItem).offset().left < value ? $(elementItem).offset().left : value
+            }
+            return value - parseFloat(elementTarget.padding.split(' ')[1].replace('px'))
         }
         return $(elementTarget.target).offset().left - parseFloat(elementTarget.padding.split(' ')[1].replace('px'))
     }
 }
-
